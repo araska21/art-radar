66d8cdd9a923ab073bf4a59fcc177505e658f878
araska21 <dya760@gmail.com>
2026-02-13 10:35:50 +0900

    port 추가

--- backend/coord_updater.js
+++ backend/coord_updater.js
@@ -11,7 +11,8 @@ const updateCoordinates = async () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
     });
 
     try {
